a20ceafd05ed456a0534480895daf00b4dddbf32
presentKey <kim.hyeonjae4@gmail.com>
2023-03-09 00:14:33 +0900

    new: main, login 페이지 경로 설정

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './styles/normalize.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import Main from './pages/Main';
+import Login from './pages/Login';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [{ index: true, element: <Main /> }],
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
