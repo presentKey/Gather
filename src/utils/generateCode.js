771df4d9656cf78bab3cd7777775240c24242339
presentKey <kim.hyeonjae4@gmail.com>
2023-03-10 22:06:32 +0900

    new: [#4] 모임의 고유 코드 생성 함수 구현

---
+++ src/utils/generateCode.js
@@ -0,0 +1,12 @@
+export default function generateCode() {
+  const chars =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = Math.floor(Math.random() * 5 + 8);
+  let code = '';
+
+  for (let i = 0; i < length; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return code;
+}
