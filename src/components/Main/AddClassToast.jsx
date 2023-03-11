14710899ccfc21b1a5207f541b4fd06c083c3e40
presentKey <kim.hyeonjae4@gmail.com>
2023-03-12 00:02:42 +0900

    design: 버튼의 active 상태 스타일 변경

--- src/components/Main/AddClassToast.jsx
+++ src/components/Main/AddClassToast.jsx
@@ -12,7 +12,7 @@ export default function AddClassToast() {
       <AiFillPlusCircle className={styles.plus} />
       <div className={styles['btn-group']}>
         <button
-          className={styles['create-class']}
+          className={`${styles.btn} ${content.create && styles['is-active']}`}
           type="button"
           data-type="create"
           onClick={handleContent}
@@ -20,7 +20,9 @@ export default function AddClassToast() {
           모임 만들기
         </button>
         <button
-          className={styles['participation-class']}
+          className={`${styles.btn} ${
+            content.participation && styles['is-active']
+          }`}
           type="button"
           data-type="participation"
           onClick={handleContent}
