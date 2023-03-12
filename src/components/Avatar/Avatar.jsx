25e748a1624cfcc7b5e76bac32ddf577c0d0919c
presentKey <kim.hyeonjae4@gmail.com>
2023-03-13 04:38:53 +0900

    design: 아바타 이미지와 크기 변경

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { MdFace } from 'react-icons/md';
+import { HiOutlineUserCircle } from 'react-icons/hi';
 import styles from './Avatar.module.css';
 
 export default function Avatar({ image }) {
   return (
     <div className={styles.avatar}>
       {image && <img className={styles.image} src={image} alt="아바타" />}
-      {!image && <MdFace className={styles['none-image']} />}
+      {!image && <HiOutlineUserCircle className={styles['none-image']} />}
     </div>
   );
 }
