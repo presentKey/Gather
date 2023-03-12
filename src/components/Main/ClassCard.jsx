278621199e74346cd31a891935ffd15b2101014a
presentKey <kim.hyeonjae4@gmail.com>
2023-03-13 06:09:53 +0900

    new: 모임 카드 클릭 시, 페이지 이동

--- src/components/Main/ClassCard.jsx
+++ src/components/Main/ClassCard.jsx
@@ -4,6 +4,7 @@ import { getClassDetail } from '../../api/firebase';
 import Avatar from '../Avatar/Avatar';
 import styles from './ClassCard.module.css';
 import showMax7Members from '../../utils/showMax7Members';
+import { Link } from 'react-router-dom';
 
 export default function ClassCard({ code }) {
   const { isLoading, data: myClass } = useQuery(
@@ -20,26 +21,28 @@ export default function ClassCard({ code }) {
   return (
     <>
       {myClass && (
-        <li className={styles.card}>
-          <h2 className={styles.title}>{myClass.title}</h2>
-          <dl>
-            <dt className={styles.bank}>{myClass.account.bank}</dt>
-            <dd className={styles.number}>{myClass.account.number}</dd>
-          </dl>
-          <div className={styles['member-content']}>
-            <ul className={styles['member-list']}>
-              {members.map((member) => (
-                <Avatar key={member.uid} image={member.photoURL} />
-              ))}
-            </ul>
-            {overLength !== 0 && <b>+{overLength}</b>}
-          </div>
-          <div className={styles['right']}>
-            <span
-              className={styles.total}
-            >{`${myClass.total.toLocaleString()}원`}</span>
-          </div>
-        </li>
+        <Link to="/class" className={styles.card}>
+          <li className={styles['card-list']}>
+            <h2 className={styles.title}>{myClass.title}</h2>
+            <dl>
+              <dt className={styles.bank}>{myClass.account.bank}</dt>
+              <dd className={styles.number}>{myClass.account.number}</dd>
+            </dl>
+            <div className={styles['member-content']}>
+              <ul className={styles['member-list']}>
+                {members.map((member) => (
+                  <Avatar key={member.uid} image={member.photoURL} />
+                ))}
+              </ul>
+              {overLength !== 0 && <b>+{overLength}</b>}
+            </div>
+            <div className={styles['right']}>
+              <span
+                className={styles.total}
+              >{`${myClass.total.toLocaleString()}원`}</span>
+            </div>
+          </li>
+        </Link>
       )}
     </>
   );
