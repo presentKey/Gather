bcec330e00dda9587022cde8b49df2afe1dc5f08
presentKey <kim.hyeonjae4@gmail.com>
2023-03-18 23:59:28 +0900

    new: 내역 추가 버튼의 마크업 및 스타일

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -3,9 +3,13 @@ import { useLocation } from 'react-router-dom';
 import ShowHeader from './ShowHeader';
 import ModificationHeader from './ModificationHeader';
 import { Navigate } from 'react-router-dom';
+import { HiOutlinePlusCircle } from 'react-icons/hi';
+import useInput from '../../hooks/useInput';
+import styles from './Detail.module.css';
 
 export default function Detail() {
   const { state } = useLocation();
+  const [info, handleChange] = useInput({ type: 'deposit' });
   const [isModification, setIsModification] = useState(false);
   const handleModifyBtnClick = () => setIsModification(!isModification);
 
@@ -28,7 +32,56 @@ export default function Detail() {
           onModifyBtnClick={handleModifyBtnClick}
         />
       )}
-      <div>Detail</div>
+      <section className={styles.detail}>
+        <button className={styles['plus-btn']} type="button">
+          <HiOutlinePlusCircle />
+        </button>
+        <form className={styles.form}>
+          <div className={styles['radio-group']}>
+            <input
+              type="radio"
+              id="deposit"
+              name="type"
+              value="deposit"
+              onChange={handleChange}
+              checked={info.type === 'deposit'}
+            />
+            <label htmlFor="deposit">입금</label>
+            <input
+              type="radio"
+              id="withdraw"
+              name="type"
+              value="withdraw"
+              onChange={handleChange}
+              checked={info.type === 'withdraw'}
+            />
+            <label htmlFor="withdraw">출금</label>
+          </div>
+          <div className={styles['price-group']}>
+            <label htmlFor="price">금액</label>
+            <input
+              type="text"
+              id="price"
+              name="price"
+              value={info.price ?? ''}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles['date-group']}>
+            <label htmlFor="date">날짜</label>
+            <input type="date" required />
+          </div>
+          <div className={styles['btn-group']}>
+            <button className={styles['register-btn']} type="button">
+              등록
+            </button>
+            <button className={styles['cancel-btn']} type="button">
+              취소
+            </button>
+          </div>
+        </form>
+      </section>
     </>
   );
 }
