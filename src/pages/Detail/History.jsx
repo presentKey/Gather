a3d31969e8cb399b0bbe9cb3210cd7be585b7ee2
presentKey <kim.hyeonjae4@gmail.com>
2023-03-20 00:14:29 +0900

    design: 입출금 내역 style 적용

--- src/pages/Detail/History.jsx
+++ src/pages/Detail/History.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import Avatar from '../../components/common/Avatar/Avatar';
 import { v4 as uuidv4 } from 'uuid';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import styles from './History.module.css';
 
 export default function History({ history, members }) {
   const { date, price, type, uid } = history;
 
   return (
-    <li>
-      <div>{date}</div>
-      <div>{price}</div>
-      <div>{type}</div>
-      <div>{uid}</div>
-      {members.map(
-        (members) =>
-          members.uid === uid && (
-            <Avatar key={uuidv4()} image={members.photoURL} />
-          )
-      )}
-      <br />
+    <li className={styles['history-list']}>
+      <div className={styles.left}>
+        {members.map(
+          (member) =>
+            member.uid === uid && (
+              <Avatar key={uuidv4()} image={member.photoURL} />
+            )
+        )}
+        <span className={styles.date}>{date}</span>
+      </div>
+      <div className={styles.right}>
+        <b className={`${styles.type} ${type === 'deposit' && styles.deposit}`}>
+          {type === 'deposit' ? '입금' : '출금'}
+        </b>
+        <strong className={styles.price}>{price.toLocaleString()}원</strong>
+      </div>
+      <button className={styles['set-btn']} type="button">
+        <BsThreeDotsVertical className={styles['set-icon']} />
+      </button>
     </li>
   );
 }
