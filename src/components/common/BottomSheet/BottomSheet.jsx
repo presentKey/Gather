8fa20eb1b0cd4128719f37b78ed55d90ffdb3e12
presentKey <kim.hyeonjae4@gmail.com>
2023-05-03 22:32:04 +0900

    new: BottomSheet 컴포넌트의 구조 변경

--- src/components/common/BottomSheet/BottomSheet.jsx
+++ src/components/common/BottomSheet/BottomSheet.jsx
@@ -1,47 +1,13 @@
-import { AiFillPlusCircle } from 'react-icons/ai';
-import styles from './BottomSheet.module.css';
-import ButtonGroup from './ButtonGroup/ButtonGroup';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { throttle } from 'lodash';
+import Button from '../Button/Button';
+import Input from '../input/Input';
+import Form from './Form/Form';
+import Header from './Header/Header';
 
-export default function BottomSheet({
-  children,
-  content,
-  handleContent,
-  buttonInfo,
-  height,
-  setHeaderHeight,
-}) {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const aside = useRef(null);
-  const handleResize = useCallback(
-    throttle(() => {
-      setScreenWidth(window.innerWidth);
-      setHeaderHeight(aside.current?.offsetHeight);
-    }, 700),
-    []
-  );
-
-  useEffect(() => {
-    setHeaderHeight(aside.current?.offsetHeight);
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, [screenWidth]);
-
-  return (
-    <>
-      <aside
-        className={styles.sheet}
-        style={{
-          transform: `translate3d(${screenWidth >= 768 ? '-50%' : '0'}, -${height}px, 0)`,
-        }}
-        ref={aside}
-      >
-        <AiFillPlusCircle className={styles.plus} />
-        <ButtonGroup content={content} handleContent={handleContent} buttonInfo={buttonInfo} />
-        {children}
-      </aside>
-    </>
-  );
+export default function BottomSheet({ children }) {
+  return <aside>{children}</aside>;
 }
+
+BottomSheet.Header = Header;
+BottomSheet.Form = Form;
+BottomSheet.Button = Button;
+BottomSheet.Input = Input;
