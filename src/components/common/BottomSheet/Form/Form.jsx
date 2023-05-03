fdeaba93beaff4a44c50b7c37f82342ab07a5b76
presentKey <kim.hyeonjae4@gmail.com>
2023-05-04 01:37:28 +0900

    design: BottomSheet 스타일 조정

--- src/components/common/BottomSheet/Form/Form.jsx
+++ src/components/common/BottomSheet/Form/Form.jsx
@@ -1,3 +1,13 @@
+import styles from './Form.module.css';
+import Symbole from '../../icons/Symbole';
+
 export default function Form({ children }) {
-  return <form>{children}</form>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>{children}</form>
+      <div className={styles.icon}>
+        <Symbole large />
+      </div>
+    </div>
+  );
 }
