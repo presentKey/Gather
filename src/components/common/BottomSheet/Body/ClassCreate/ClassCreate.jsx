1377067bc20fa2fceb641d075708ec325c280f0a
presentKey <kim.hyeonjae4@gmail.com>
2023-05-08 01:33:15 +0900

    new: CheckBox 구현

--- src/components/common/BottomSheet/Body/ClassCreate/ClassCreate.jsx
+++ src/components/common/BottomSheet/Body/ClassCreate/ClassCreate.jsx
@@ -1,11 +1,12 @@
 import styles from '../BottomSheetForm.module.css';
 import Button from '../../../Button/Button';
-import Input from '../../../input/Input';
+import Input from '../../../Input/Input';
 import { useBottomSheet } from '../../../../../context/BottomSheetContext';
 import useInput from '../../../../../hooks/useInput';
 import useClass from '../../../../Main/hooks/useClass';
+import CheckBox from '../../../Input/CheckBox';
 
-export default function ClassCreate({ distinct, onSubmit }) {
+export default function ClassCreate({ distinct }) {
   const { toggle } = useBottomSheet();
   const [info, handleChange] = useInput();
   const { isLoading, error, handleSubmit } = useClass();
@@ -13,6 +14,12 @@ export default function ClassCreate({ distinct, onSubmit }) {
   if (toggle !== distinct) return;
   return (
     <form className={styles.form} onSubmit={(e) => handleSubmit(e, info, distinct)}>
+      <CheckBox
+        type='checkbox'
+        name='allowAnonymouse'
+        text='모임 구성원으로 게스트 유저 허용'
+        onChange={handleChange}
+      />
       <Input type='text' name='title' text='모임 이름' value={info} onChange={handleChange} />
       <Input type='text' name='bank' text='은행' value={info} onChange={handleChange} />
       <Input type='number' name='number' text='계좌번호' value={info} onChange={handleChange} />
