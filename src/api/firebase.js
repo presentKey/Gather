d2f34312c9e92cfef55664e5f180e62fba109fcc
presentKey <kim.hyeonjae4@gmail.com>
2023-05-16 23:28:38 +0900

    refactor: 환경변수 오타 수정

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -34,7 +34,7 @@ import sortHistory from '../utils/sortHistory';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_I,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 const provider = new GoogleAuthProvider();
 const app = initializeApp(firebaseConfig);
