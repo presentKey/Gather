0e6e2ed73f7434c0157b60d48cb3de6dd4172f0b
presentKey <kim.hyeonjae4@gmail.com>
2023-05-24 01:02:05 +0900

    new: [#44] 기존 오류 알림을 Toast Notification으로 변경

--- src/components/common/BottomSheet/Body/ClassCreateForm.jsx
+++ src/components/common/BottomSheet/Body/ClassCreateForm.jsx
@@ -5,6 +5,8 @@ import { useBottomSheet } from '../../../../context/BottomSheetContext';
 import useInput from '../../../../hooks/useInput';
 import CheckBox from '../../Input/CheckBox';
 import useClassList from '../../../../hooks/useClassList';
+import ModalPortal from '../../Modal/ModalPortal';
+import ToastNotification from '../../Modal/ToastNotification';
 
 export default function ClassCreateForm({ tag }) {
   const { toggle } = useBottomSheet();
@@ -13,31 +15,39 @@ export default function ClassCreateForm({ tag }) {
 
   if (toggle !== tag) return;
   return (
-    <form className={styles.form} onSubmit={(e) => handleCreateSubmit(e, info)}>
-      <CheckBox
-        type='checkbox'
-        name='allowAnonymouse'
-        text='모임 구성원으로 게스트 유저 허용'
-        onChange={handleChange}
-      />
-      <Input
-        type='text'
-        name='title'
-        text='모임 이름'
-        value={info}
-        onChange={handleChange}
-        required
-      />
-      <Input type='text' name='bank' text='은행' value={info} onChange={handleChange} required />
-      <Input
-        type='number'
-        name='number'
-        text='계좌번호'
-        value={info}
-        onChange={handleChange}
-        required
-      />
-      <Button text='모임 생성!' type='submit' color='yellow' isLoading={isLoading} />
-    </form>
+    <>
+      <form className={styles.form} onSubmit={(e) => handleCreateSubmit(e, info)}>
+        <CheckBox
+          type='checkbox'
+          name='allowAnonymouse'
+          text='모임 구성원으로 게스트 유저 허용'
+          onChange={handleChange}
+        />
+        <Input
+          type='text'
+          name='title'
+          text='모임 이름'
+          value={info}
+          onChange={handleChange}
+          required
+        />
+        <Input type='text' name='bank' text='은행' value={info} onChange={handleChange} required />
+        <Input
+          type='number'
+          name='number'
+          text='계좌번호'
+          value={info}
+          onChange={handleChange}
+          required
+        />
+        <Button text='모임 생성!' type='submit' color='yellow' isLoading={isLoading} />
+      </form>
+
+      {error.state && (
+        <ModalPortal>
+          <ToastNotification message={error.message} />
+        </ModalPortal>
+      )}
+    </>
   );
 }
