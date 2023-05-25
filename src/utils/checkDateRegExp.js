7b74ecc9843716eb1ecd46f710e20404441808c2
presentKey <kim.hyeonjae4@gmail.com>
2023-05-25 21:10:15 +0900

    docs: utils 함수 주석 추가

--- src/utils/checkDateRegExp.js
+++ src/utils/checkDateRegExp.js
@@ -1,3 +1,8 @@
+/**
+ * 날짜 정규식 검사
+ * @example 2023-05-25
+ * @param date 날짜
+ */
 export default function checkDateRegExp(date) {
   return /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/.test(date);
 }
--- src/utils/isMobile.js
+++ src/utils/isMobile.js
@@ -1,3 +1,7 @@
+/**
+ * 접속 기기 판별 함수
+ * @desc 모바일 기기에서 Firefox 제외
+ * */
 export default function isMobile() {
   if (/Firefox/i.test(navigator.userAgent)) return;
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
