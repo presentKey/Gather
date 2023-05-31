16a3fdb1f273d64b04b23cea6863389079b1ddc2
presentKey <kim.hyeonjae4@gmail.com>
2023-06-01 05:11:40 +0900

    new: Detail 페이지에서 뒤로가기 버튼 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,25 @@ import styles from './css/Header.module.css';
 import { Link } from 'react-router-dom';
 import { useAuthContext } from '../context/AuthContext';
 import Avatar from './common/Avatar';
+import { useLocation } from 'react-router-dom';
+import { ImArrowLeft2 } from 'react-icons/im';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
   const { user, logout } = useAuthContext();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   return (
     <header className={styles.header}>
-      <Avatar image={user.photoURL} />
+      <div className={styles.container}>
+        {pathname === '/detail' && (
+          <button className={styles['back-button']} type='button'>
+            <ImArrowLeft2 onClick={() => navigate('/')} />
+          </button>
+        )}
+        <Avatar image={user.photoURL} />
+      </div>
       <Link to='/' className={styles.title}>
         Gather
       </Link>
