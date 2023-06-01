9861cb06067b5f6c41159dff037188f25b27214a
presentKey <kim.hyeonjae4@gmail.com>
2023-06-01 17:45:56 +0900

    new: 계좌번호 복사 함수 구현

--- src/utils/clipboard.js
+++ src/utils/clipboard.js
@@ -1,12 +1,20 @@
 /**
- * 클립보드 text 내용
+ * 모임 코드 복사
  * @param code 모임의 고유 코드
  * @param closeModal 모달 닫기
  */
-export default function clipboard(code, closeModal) {
+export function copyCode(code, closeModal) {
   const text = `https://warm-kulfi-2043c3.netlify.app/
 모임 코드: ${code}`;
   window.navigator.clipboard
     .writeText(text) //
     .then(closeModal);
+}
+
+/**
+ * 계좌번호 복사
+ * @param number 계좌번호
+ */
+export function copyAccountNumber(number) {
+  window.navigator.clipboard.writeText(number);
 }
